11cdb8c6a059eb30c12910c645aaccf878c6fd8e
Raghvi Saxena <201951119@iiitvadodara.ac.in>
2020-12-14 21:35:51 +0530

    registration form complete

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']= 'a60773a584fe792fd8ec9061e7ca6c00'
 
 posts=[
     {
@@ -24,6 +27,21 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__=='__main__':
     app.run(debug=True)

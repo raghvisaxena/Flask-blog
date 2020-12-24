1b7b592f34a38a6ea5365f7cf615cba2b421d7bd
Raghvi Saxena <201951119@iiitvadodara.ac.in>
2020-12-24 13:09:15 +0530

    Database with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask import Flask,render_template,url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer , primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -22,11 +22,12 @@ class User(db.model):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class Post(db.model):
+class Post(db.Model):
     id = db.Column(db.Integer , primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
